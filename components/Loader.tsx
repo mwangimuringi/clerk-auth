1b2c4d8fc38b7fbaf88e68b870a43ac4186bb983
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-01 14:07:40 +0300

    Added starter files

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className="flex items-center justify-center hc w-full bg-background z-20">
-            
+            <data value="Loading..." className="text-xl font-bold text-white"></data>
             <div className="loader w-10 h-10"></div>
         </div>
     )
