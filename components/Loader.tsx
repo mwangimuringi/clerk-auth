c89be2d9e6c9d6d82c145fd813f96813d3138859
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-01 13:59:18 +0300

    ...

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className="flex items-center justify-center hc w-full bg-background z-20">
+            
             <div className="loader w-10 h-10"></div>
         </div>
     )
