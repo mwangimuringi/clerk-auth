32dcf3cf53aa42e900273046f2f500c8c8462178
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-01 14:08:06 +0300

    Styled the navbar

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -5,6 +5,8 @@ const Loader = () => {
         <div className="flex items-center justify-center hc w-full bg-background z-20">
             <data value="Loading..." className="text-xl font-bold text-white"></data>
             <div className="loader w-10 h-10"></div>
+            <div className="loader w-10 h-10"></div>
+            <div className="loader w-10 h-10"></div>
         </div>
     )
 };
